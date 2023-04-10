afa32c61f884181529b03560c194709699ededf5
asad <asadzahid1479@gmail.com>
2023-04-10 23:56:47 +0500

    setting up project

---
+++ src/main/java/com/nerdware/onlinemarketplacesecuritysystem/OnlineMarketplaceSecuritySystemApplication.java
@@ -0,0 +1,13 @@
+package com.nerdware.onlinemarketplacesecuritysystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineMarketplaceSecuritySystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineMarketplaceSecuritySystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nerdware/onlinemarketplacesecuritysystem/OnlineMarketplaceSecuritySystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nerdware.onlinemarketplacesecuritysystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineMarketplaceSecuritySystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
